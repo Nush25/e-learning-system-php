c6034df9e96e291cc30b63682119a64444a621fb
Anushka Patil <anushkapatil525@gmail.com>
2026-03-10 11:39:19 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,15 @@
+<html>
+  <style>
+    body{
+      background-image:url("images/bcimg.png");
+      background-size:cover;
+      background-position:center;
+    }
+    </style>
+    <body>
+<h1><?php echo $title;?></h1>
+<p>Our School is Nursery to 8 standard, which began in June 2008. Vivekanand Pratishthan English Medium School is an initiative of the Vivekanand Pratishthan Foundation and aim to provide world-class education focusing on the holistic development of young learners. The school is a contemporary urban school and a part of Vivekanand Pratishthan.
+The school began its first year with 50 student, and will open its 8th Academic Year on August 5,2015 with approximately now it reached 1005 students.
+The school is governed by a 17 member board of directors. Knowledge, Respect, Social Responsibility, Creativity and celebration man making education, learning and sharing continuously, responsibility, achieving Excellency, nationalism and patriotism, social, sensitivity, quality education, team work are the core values which the school decided to adopt as a part of its identity.</p>
+    </body>
+</html>
